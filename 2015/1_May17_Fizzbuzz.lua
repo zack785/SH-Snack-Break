136d91873f4b1cbc4966763ce5cb98440fcb995d
Sukinahito <Sukinahito@users.noreply.github.com>
2015-05-12 13:41:16 -0400

    Added May 17th, 2015 problem

---
+++ 2015/1_May17_Fizzbuzz.lua
@@ -0,0 +1,12 @@
+--[[
+-- ScriptingHelpers Snack Break
+-- The week of May 17th, 2015
+-- 
+-- Problem:
+-- Write a program that prints the numbers from 1 to 100. But for multiples of 
+-- three print “Fizz” instead of the number and for multiples of five print 
+-- “Buzz”. For numbers which are multiples of both three and five print 
+-- “FizzBuzz” instead.
+]]--
+
+-- Solution to be posted on May 24th, 2015
