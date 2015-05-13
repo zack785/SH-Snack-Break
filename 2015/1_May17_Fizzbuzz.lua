3b91d349174cf30cdc9f3b8003407269221e2d75
Sukinahito <Sukinahito@users.noreply.github.com>
2015-05-13 10:06:26 -0400

    correction
    forgot final clause.

--- 2015/1_May17_Fizzbuzz.lua
+++ 2015/1_May17_Fizzbuzz.lua
@@ -6,7 +6,8 @@
 -- Write a program that prints the numbers from 1 to 100. But for multiples of 
 -- three print “Fizz” instead of the number and for multiples of five print 
 -- “Buzz”. For numbers which are multiples of both three and five print 
--- “FizzBuzz” instead.
+-- “FizzBuzz” instead. If the number is not divisible by either number, print
+-- out the number.
 ]]--
 
 -- Solution to be posted on May 24th, 2015
